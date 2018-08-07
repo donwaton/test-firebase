5a8f9a01683b23427c4233816e9ba209497e1dd0
Héctor Cifuentes <hcifuentesquijada@gmail.com>
2018-08-07 10:01:47 -0400

    CRUD Firebase

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+
+//firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { environment } from '../environments/environment';
+
+//componentes
+import { ProductsComponent } from './components/products/products.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { ProductComponent } from './components/products/product/product.component'
+
+//services
+import { ProductService } from './services/product.service'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ProductListComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FormsModule
+  ],
+  providers: [
+    ProductService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
